d42ad0709f552a2bee97e756ec3f2f0e63bce660
Sameque Santana <samequesantana@gmail.com>
2025-08-22 20:47:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -116,6 +116,8 @@ io.on("connection", (socket) => {
 });
 
 const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
+//server.listen(PORT, () => {
+//    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+//});
+
+module.exports = sever;
